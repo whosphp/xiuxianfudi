82e6106f1b37d87b4d5ce8c85a2b2f6d36ba7e2c
whosphp <11045186+whosphp@users.noreply.github.com>
2019-12-31 16:49:58 +0800

    fix

--- xx.js
+++ xx.js
@@ -198,7 +198,7 @@
         }
     })
 
-    var host = 'http://xx.gl.test'
+    var host = 'http://xx.gl.test:9102'
 
     function who_log_success (msg) {
         console.debug('%c'+msg, 'color: green; font-size: 16px;')
