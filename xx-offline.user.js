7f5a95587fc2ddb522a1efdfea0e732fbbda2311
whosphp <11045186+whosphp@users.noreply.github.com>
2020-01-21 12:09:21 +0800

    socket 连接成功之后先离开队伍

--- xx-offline.user.js
+++ xx-offline.user.js
@@ -65,6 +65,9 @@ let who_interval = setInterval(function () {
             if (socket === undefined || !socket) {
                 old(index)
             } else if (socket.connected) {
+                // socket 连接成功之后先离开队伍
+                leaveTeamFunc()
+
                 socket.off('disconnect')
                 socket.on('disconnect', function () {
                     who_notify('disconnect', 1)
